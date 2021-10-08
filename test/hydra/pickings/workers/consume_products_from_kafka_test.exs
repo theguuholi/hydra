c0e4e54795c713e0fcf243814213fc0725b771ae
Gustavo <g.92oliveira@gmail.com>
2021-10-08 13:15:34 -0300

    test with mock

--- test/hydra/pickings/workers/consume_products_from_kafka_test.exs
+++ test/hydra/pickings/workers/consume_products_from_kafka_test.exs
@@ -1,30 +1,21 @@
-# defmodule Hydra.Pickings.Workers.ConsumeProductsFromKafkaTest do
-#   use ExUnit.Case, async: true
-#   alias Hydra.Pickings.Workers.ConsumeProductsFromKafka
-#   alias Hydra.Pickings
-#   import Mock
+defmodule Hydra.Pickings.Workers.ConsumeProductsFromKafkaTest do
+  use ExUnit.Case, async: true
+  alias Hydra.Pickings
+  import Mock
 
-#   test "test message" do
-#     product = %{
-#       "products" => [
-#         %{
-#           "product" => "abobora",
-#           "stores" => ["123"]
-#         }
-#       ]
-#     }
+  test "test message" do
+    product = %{
+      "products" => [
+        %{
+          "product" => "abobora",
+          "stores" => ["123"]
+        }
+      ]
+    }
 
-#     with_mock(Pickings,
-#     store_pickings_into_mongo: fn _ ->
-#         [%Mongo.InsertOneResult{acknowledged: true, inserted_id: "123213"}]
-#       end
-#     ) do
-#       # ref = Broadway.test_message(:picking_consumer, Jason.encode!(product))
-#       ref = Broadway.test_batch(:picking_consumer, [Jason.encode!(product)], batch_mode: :flush)
-#       IO.inspect(ref)
-#       assert_receive {:ack, ^ref, [], []}, 1000
-#       # assert_receive {:DOWNk, ^ref, _, _, :normal}
-
-#     end
-#   end
-# end
+    with_mock(Pickings, store_pickings_into_mongo: fn _ -> :ok end) do
+      ref = Broadway.test_batch(:picking_consumer, [Jason.encode!(product)], batch_mode: :flush)
+      assert_receive {:ack, ^ref, _, _}, 1000
+    end
+  end
+end
